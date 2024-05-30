fa8ddc5a4f9ee78015b103b0dd280e20b2c01dd9
sohamroy589@ <roysoham58@gmail.com>
2024-05-31 02:29:35 +0530

    adds step function invocation

---
+++ randgen-java/src/main/java/org/example/StepFnInvocator.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import com.google.gson.Gson;
+import org.example.dao.RandomNumberGenerationTask;
+import software.amazon.awssdk.services.sfn.SfnClient;
+import software.amazon.awssdk.services.sfn.model.StartExecutionRequest;
+import software.amazon.awssdk.services.sfn.model.StartExecutionResponse;
+import java.util.UUID;
+
+public class StepFnInvocator {
+    private final SfnClient sfnClient;
+    private final String stateMachineArn;
+
+    StepFnInvocator(SfnClient sfnClient, String stateMachineArn) {
+        this.sfnClient = sfnClient;
+        this.stateMachineArn = stateMachineArn;
+    }
+
+    public StartExecutionResponse invoke(RandomNumberGenerationTask task) {
+        StartExecutionRequest executionRequest = StartExecutionRequest.builder()
+                .input(new Gson().toJson(task))
+                .stateMachineArn(stateMachineArn)
+                .name(UUID.randomUUID().toString())
+                .build();
+
+        return sfnClient.startExecution(executionRequest);
+    }
+}
